5fbd7a74098cd523f1f6a254c3908e99a7cbfe76
Arthi1904 <64977517+Arthi1904@users.noreply.github.com>
2020-06-10 15:02:05 +0530

    Create 001-stages-final.groovy

---
+++ jenkinsfiles/001-stages-final.groovy
@@ -0,0 +1,31 @@
+pipeline {
+    environment {
+    registry = "9962281904/docker-test"
+    registryCredential = 'dockerhub'
+    dockerImage = ''
+  }
+  agent any
+  stages {
+    stage('Cloning Git') {
+      steps {
+        git url: 'https://github.com/Arthi1904/jenkins-examples.git', branch: 'master'
+      }
+    }
+    stage('Building image') {
+      steps{
+        script {
+          dockerImage = docker.build registry + ":$BUILD_NUMBER"
+        }
+      }
+    }
+    stage('Deploy Image') {
+      steps{
+        script {
+          docker.withRegistry( '', registryCredential ) {
+            dockerImage.push()
+          }
+        }
+      }
+    }
+  }
+}
